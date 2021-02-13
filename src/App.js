ef65adcc01c8e3cf19a84ee4248cd784cf4a6c4e
Rich G <rich@cloudnost.com>
2021-02-13 06:20:33 +0000

    fixed typo on image line

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-	  <img src={logo} className="App-logo"
+	  <img src={logo} className="App-logo">
         <p>
           React GitHub Page...
         </p>
